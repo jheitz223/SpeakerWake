fdd3c585d151950999b363517eec3f0752dbca2d
jheitz223 <jheitz223@gmail.com>
2020-10-31 02:58:51 -0400

    Add files via upload

---
+++ Source.cs
@@ -0,0 +1,13 @@
+﻿class SpeakerWake {
+
+	static void Main() {
+
+		System.Media.SoundPlayer Audio = new System.Media.SoundPlayer(@"C:\Program Files\SpeakerWake\noise.wav");
+		while (true) {
+			Audio.Play();
+			System.Threading.Thread.Sleep(60000);
+		}
+
+	}
+
+}
